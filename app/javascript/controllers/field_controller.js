64378b3a849597380566097314ca487cf8ce752b
Niclas Collmer <niclas.collmer@9elements.com>
2022-06-25 16:11:29 +0200

    auto increment of field name and id

--- app/javascript/controllers/field_controller.js
+++ app/javascript/controllers/field_controller.js
@@ -8,16 +8,25 @@ export default class extends Controller {
   ];
 
   add_field() {
+    this.check_index()
+
     var fieldContainer = document.createElement("div")
     fieldContainer.classList.add("form-container__group")
 
     var inputField = document.createElement("input")
     inputField.name = "seminar_instance[seminar_contents_attributes][" + field_index + "][content]"
+    inputField.id = "seminar_instance_seminar_contents_attributes_" + field_index + "_content"
     inputField.type = "text"
 
     fieldContainer.append(inputField)
-    field_index = field_index + 1
 
     this.field_containerTarget.append(fieldContainer);
+  }
+
+  check_index() {
+    if(document.getElementById("seminar_instance_seminar_contents_attributes_" + field_index + "_content") != null) {
+      field_index = field_index + 1
+      this.check_index()
+    };
   }
 }
