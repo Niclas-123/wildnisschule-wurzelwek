dc6f2cf04b767c7056c23f0fb1295d46baf04133
Niclas Collmer <niclas.collmer@9elements.com>
2022-06-25 15:59:00 +0200

    custom dynamic form implemented

--- app/javascript/controllers/field_controller.js
+++ app/javascript/controllers/field_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
+let field_index = 1
+
 export default class extends Controller {
   static targets = [
     "field_container"
@@ -7,9 +9,14 @@ export default class extends Controller {
 
   add_field() {
     var fieldContainer = document.createElement("div")
-    var inputField = document.createElement("input")
     fieldContainer.classList.add("form-container__group")
+
+    var inputField = document.createElement("input")
+    inputField.name = "seminar_instance[seminar_contents_attributes][" + field_index + "][content]"
+    inputField.type = "text"
+
     fieldContainer.append(inputField)
+    field_index = field_index + 1
 
     this.field_containerTarget.append(fieldContainer);
   }
