6e82c5fe6bfbd5098704ff6cc0ab09c1b6f8a50a
Niclas Collmer <niclas.collmer@9elements.com>
2022-07-16 09:42:10 +0200

    texts updated

--- db/seeds/01_seminar_types.rb
+++ db/seeds/01_seminar_types.rb
@@ -1,6 +1,6 @@
 SeminarType.new(name: "Fortbildungen", description: "Fortbildungen sind Wochenenden, die sich einem speziellen Thema widmen um dieses zu vertiefen. Dabei spielen schon vorhandene Kenntnisse nur eine untergeordnete Rolle.").save(validate: false)
 
-SeminarType.new(name: "Wanderungen", description: "Wanderungen folgen einem Urinstinkt des Menschen - zu nomadisieren und umherzustreifen. Deshalb werden diese Tage gerne auch in innerer Ruhe verbracht, um ganz Ohr und Nase für die Natur zu sein.").save(validate: false)
+SeminarType.new(name: "Wanderungen", description: "Unsere Wanderungen folgen einem Urinstinkt des Menschen - wie  Nomaden umherzustreifen, einem inneren Ruf folgend. Deshalb werden diese Tage gerne auch in  Stille verbracht um ganz Ohr und Nase für die Natur zu sein. Dadurch unterscheiden sie sich von üblichen Wanderungen, bei denen im Nachhinein von der uns umgebenden Natur kaum etwas wahrgenommen wurde. Es werden dabei unterschiedliche Schwerpunkte gelegt wie z.B. Spurenlesen, Naturmentoring, Meditation und Erweiterung der Sinne.").save(validate: false)
 
 SeminarType.new(name: "Übergangsrituale", description: "Übergangsrituale, besonders für Jugendliche, wurden schon immer und überall praktiziert und werden in der heutigen Zeit immer wichtiger, um den Weg vom Kind über den Jugendlichen zum Erwachsenen zu würdigen.").save(validate: false)
 
--- db/seeds/03_seminar_instances.rb
+++ db/seeds/03_seminar_instances.rb
@@ -4,10 +4,18 @@ s = SeminarInstance.new(
   description: "Seit jeher bilden Wildpflanzen  zu 80% die Nahrungsgrundlage für den Menschen,höchstens 20% stammen von tierischen Quellen.Das Wissen um die Eßbarkeit  schlummert tief im Verborgenen von jedem von uns,und das Vertrauen in das sichere Erkennen kann wieder wachsen.Wir legen die Grundlagen für das Bestimmen der wichtigsten eßbaren Kräuter ,stellen einfaches Kochgeschirr her,knüpfen ein Band und genießen unsere grünen Wilden mit dem Gaumen .Auch der Heilaspekt,die grüne Apotheke der Natur,wird nicht zu kurz kommen,können wir doch wie jedes Wildtier uns die Heilkraft der Pflanzen zunutze machen.Spielerisch und nachhaltig verwurzeln wir die Grundlagen,sodaß ein jeder auch nach dem Seminar sich an das Erlernte erinnern kann.",
   price_cents: 10000
 )
-
 s.image_1.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
 s.image_2.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
 s.save!
+s.seminar_content.create(content: "eßbare Wildpflanzen sicher bestimmen und erkennen")
+s.seminar_content.create(content: "Pflanzenfamilien zuordnen")
+s.seminar_content.create(content: "dadurch auf Inhaltsstoffe schließen")
+s.seminar_content.create(content: "Kochgeschirr herstellen")
+s.seminar_content.create(content: "Schnüre drillen")
+s.seminar_content.create(content: "Spiele zum Vertiefen des Wissens")
+s.seminar_content.create(content: "Heilpflanzen kennenlernen")
+
+
 
 s = SeminarInstance.new(
   name: "Die wunderbare Welt der Vögel",
@@ -16,13 +24,19 @@ s = SeminarInstance.new(
   Um unsere Verbindung zu ihnen und unsere Wahrnehmung für sie zu vertiefen lassen wir uns berühren -im Innen und im Außen .und lernen ganz nebenbei noch mit den Kleinen die ganz große universelle  Sprache der Natur kennen .Wir lernen Vögel am Flugbild zu unterscheiden, Gesänge und verschiedene Rufarten ,Federn zuzuordnen,Schnäbel bestimmen,Ökologie und Strategie der Gefiederten.Wir erkennen Gemeinsamkeiten und Unterschiede, sodaß Grundlagen gelegt werden um später zuhause den Weg in die Vogelwelt weiterzugehen.",
   price_cents: 10000
 )
-
 s.image_1.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
 s.image_2.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
 s.save!
+s.seminar_content.create(content: "Gesänge und Rufarten unterscheiden")
+s.seminar_content.create(content: "Federkunde")
+s.seminar_content.create(content: "Flugbilder erkennen")
+s.seminar_content.create(content: "ökologische Zusammenhänge")
+s.seminar_content.create(content: "Wahrnehmungs- und Bewußtseinswerweiterung")
+s.seminar_content.create(content: "Sprache des Waldes")
+
 
 s = SeminarInstance.new(
-  name: "Ritual mit 5 Teilnehmern",
+  name: "Übergangsritual Jugendliche",
   seminar_type_id: 3,
   description: "Oft stehen wir im Leben an einer Schwelle, einem Übergang in eine neue Lebensphase, die sich mit dem Wunsch oder Drang nach Veränderung ankündigt, wie ein Fluß, der sich ein neues Bett sucht, da das alte nicht mehr seinem Strömen entspricht. Diese Veränderungen brauchen Zeit und einen Rahmen, um sich wie auf einer neu gezeichneten Landkarte zu manifestieren.
   Gerade für Jugendliche auf dem Weg ins Erwachsen-werden fehlt oft dieser Rahmen und die Suchenden wie die Begleiter - oft Eltern – fühlen sich ob der Veränderungen hilflos und überfordert.
@@ -31,15 +45,50 @@ s = SeminarInstance.new(
   Nach ausführlicher Vorbereitung mit Gesprächen, Austausch über die eigene Identität als Erwachsener, als werdende Frau oder werdender Mann, findet jeder Jugendliche seinen Platz im Wald, richtet sein Lager ein und verweilt dort mit sich und der Natur. Diese ist sehr hilfreich als Mentorin, wird mir doch im Außen gespiegelt was im Inneren stattfindet. Nach der Solozeit werden die Schützlinge gebührend empfangen und das Erlebte gefeiert und gewürdigt.",
   price_cents: 15000
 )
-
 s.image_1.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
 s.image_2.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
 s.save!
+s.seminar_content.create(content: "Auseinandersetzung mit dem Thema Erwachsen werden")
+s.seminar_content.create(content: "Reflektion der eigenen Position im Leben")
+s.seminar_content.create(content: "Thema oder Frage finden")
+s.seminar_content.create(content: "innere Einkehr")
+s.seminar_content.create(content: "Integration des Erlebten")
 
-s = SeminarInstance.new(name: "Kräuterwanderung", seminar_type_id: 2, description: "Hier lernt man Krauter", price_cents: 12000)
 
+
+s = SeminarInstance.new(
+  name: "Bäume",
+  seminar_type_id: 1,
+  description: "Sie spenden uns im Sommer Schatten, wärmen uns im Winter mit ihrem Holz, bieten uns Schutz bei Regen, reichern unsere Luft mit Sauerstoff an, geben uns Nahrung, Heilung und Halt .Sie besitzen eine nahezu unvergleichliche Regenerationsfähigkeit und Ausdauer -verbleiben sie doch ihr gesamtes Leben an einer Stelle- und sie erleben unzählige menschliche Geschicke .Das ist es wert ,sich unseren Riesen ein Wochenende zu widmen ,sie kennenzulernen und zu erkennen. So liegt der Schwerpunkt des Seminars auf dem Erfahren der Vielseitigkeit unserer Bäume .Wir kosten, riechen, fühlen sie, verwenden und erkennen sie, spielen mit ihnen und knüpfen Bande. Wir lauschen Märchen, Mythen und Geschichten und nicht zuletzt erfahren wir die Unterschiede in der Beschaffenheit verschiedener Hölzer.",
+  price_cents: 12000
+)
 s.image_1.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
 s.image_2.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
 s.save!
+s.seminar_content.create(content: "Bäume erkennen und benennen")
+s.seminar_content.create(content: "Bäume als Nahrung")
+s.seminar_content.create(content: "Heilkraft erfahren")
+s.seminar_content.create(content: "unterschiedliches Holz verarbeiten")
+s.seminar_content.create(content: "Baum als Spiegel und Mentor")
+
+
+s = SeminarInstance.new(
+  name: "Wandel",
+  seminar_type_id: 3,
+  description: "“Wo seid ihr,ihr Alten und Weisen? Ihr, die ihr schon einen so weiten Lebensweg gegangen seid? Ich brauche Euch! Ich brauche Eure Führung!”
+  So ähnlich äußerte sich eine 20-jährige auf einem Kongress der 13 Großmütter in Karlsruhe. Diese Forderung einer jungen Frau offenbart einen gesellschaftlichen Mißstand.
+  Oft fühlen wir uns an der Schwelle zum Alter einsam, körperliche Schwächen beeinträchtigen die Lebensfreude und auch die Umstellung des Hormonhaushaltes macht uns zu schaffen. Die uns umgebenden Menschen und unsere Gesellschaft begleiten diesen Wandel nur rudimentär und wir fühlen uns oft alleine, rat- und hilflos. Als wir noch in ursprünglichen Gemeinschaften lebten, wurde diese Zeit erfüllt durch die Weitergabe von Lebenserfahrung, die Führung und Begleitung der nachfolgenden Generationen und zahlreiche Aufgaben wie Kinderbetreuung, Leder gerben etc.
+  Unsere heutigen Lebensumstände ermöglichen ein solches Aufgehoben- und Getragensein im Alter oft nicht mehr und ein Gefühl des Überflüssigseins und der Sinnlosigkeit stellt sich ein. Besonders Frauen, deren Kinder das Haus verlassen, empfinden eine Art der Nestleere.
+  In diesen Tagen wollen wir uns mit phytohormonellen Pflanzen, Ernährung in den Wechseljahren und einem Medicinwalk beschäftigen, einem Schwellengang, der einen ganzen Tag dauern wird, um herauszufinden, was uns bisher genährt hat, was uns jetzt fehlt und was wir brauchen um in der weiteren Lebensphase ein erfülltes und reiches Leben zu führen. Nach dem Tag in der Natur werden wir in die Gemeinschaft aufgenommen, um am folgenden Morgen unsere Geschenke zu teilen.
+  Dadurch daß wir die Geschichten anderer Menschen hören, denen es ähnlich ergeht und die sich ähnlich fühlen, verwandelt sich oft unser Schmerz in eine Kraft, die den Mut nährt, neue Schritte zu gehen.
+  ",
+  price_cents: 12000)
+s.image_1.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
+s.image_2.attach(io: File.open("app/assets/images/owl.jpg"), filename: "Owl.jpg")
+s.save!
+s.seminar_content.create(content: "Ernährung in den Wechseljahren und Kochen eines solchen Gerichtes")
+s.seminar_content.create(content: "phytohormonelle Pflanzen kennenlernen")
+s.seminar_content.create(content: "eintägiger Schwellengang mit anschließender Integration")
+s.seminar_content.create(content: "Austausch, Geschichten der Großmütter und das Erleben von Gemeinschaft in einer Zeit des Wandels ")
 
 puts "Seminar Instances created"
